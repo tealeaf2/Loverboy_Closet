74360a727008a0b6c29eb370f56a25b5deb14efe
tealeaf2 <shadowrocket2003@gmail.com>
2024-11-07 12:53:39 -0500

    adding server stuff

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -2,17 +2,29 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
+
   modules: [
     '@nuxt/eslint',
   ],
+
   plugins: [
     '~/plugins/api.js',
     '~/plugins/element-plus.js', 
   ],
+
   runtimeConfig: {
     public: {
       baseURL: process.env.BASE_URL || 'http://127.0.0.1:5000',
       apiPath: process.env.VUE_APP_API_PATH || '/api'
     }
+  },
+
+  server: {
+    host: process.env.HOST || '0.0.0.0',
+    port: process.env.PORT || 3000,
+  },
+
+  app: {
+    baseURL: process.env.BASE_ROUTER || '/'
   },
 })
