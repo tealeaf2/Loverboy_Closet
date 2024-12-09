465a62ab4034b0194808c3741ce043c9a9df466d
Isaac <jxie4@nd.edu>
2024-12-08 19:43:36 -0500

    frontend -- closet,hero

---
+++ frontend/pages/button.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { defineProps } from 'vue';
+const props= defineProps(['text'])
+</script>
+
+<template>
+<div class="button">{{ props.text }}</div>
+</template>
+
+
+<style scoped>
+  .button{
+    width: 150px;
+    height: 50px;
+    background-color: #c07858;
+    border-radius: 8px;
+    font-size: 17px;
+    text-align: center;
+    line-height: 50px;
+    color: white;
+    font-weight: 600;
+    user-select: none;
+    cursor: pointer;
+  }
+</style>
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,10 +1,58 @@
+<script setup>
+import Header from './header.vue';
+import swiper from './swiper.vue';
+import Button from './button.vue';
+import { ref } from 'vue';
+const scrollTarget = ref(null)
+function schrollDown(){
+  scrollTarget.value.scrollIntoView({ behavior: "smooth" , block: 'end'});
+}
+</script>
+  
 <template>
-  <div>
-    <p>Home Page</p>
+  <Header></Header>
+  <div class="hero-box">
+    <h1 class="hero up">
+      <span><a>Dress </a></span>
+      <span><a>easily </a></span>
+      <span><a>and </a></span>
+    </h1>
+    <h1 class="hero down">
+      <span><a>Style </a></span>
+      <span><a>Smart.</a></span>
+    </h1>
+    <Button @click="schrollDown" text="Dress Up"></Button>
   </div>
+  <div ref="scrollTarget" >
+    <swiper class="swiper"></swiper>
+  </div>
+  
+  
+
+  
 </template>
 
-<script>
+<style scoped>
+  @import url('https://fonts.googleapis.com/css2?family=Newsreader:ital,opsz,wght@0,6..72,200..800;1,6..72,200..800&family=Sour+Gummy:ital,wght@0,100..900;1,100..900&display=swap');
+  .hero-box{
+    padding-top: 32px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+  .hero{
+    text-align: center;
+    font-family: "Newsreader", serif;
+    font-size: 5.5vw;
+    font-weight: 300;
+  }
+  .hero.down{
+    font-style: italic;
+    color: #c07858;
+    margin-top: -50px;
+  }
 
-</script>
 
+
+</style>
