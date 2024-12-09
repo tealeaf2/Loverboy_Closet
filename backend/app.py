9d186751ab36e80c76682df0d0e2d5427572ce5f
tealeaf2 <shadowrocket2003@gmail.com>
2024-12-09 17:38:19 -0500

    changed database to sqlalchemy. Note: WIP

--- backend/app.py
+++ backend/app.py
@@ -1,20 +1,33 @@
+# import os
+# from flask import Flask
+# from flask_cors import CORS
+# from views.home import posts_bp
+# from dotenv import load_dotenv
+# from flask_sqlalchemy import SQLAlchemy
+
+# load_dotenv()
+# os.environ['ENV'] = 'PRODUCTION'
+
+# app = Flask(__name__)
+# CORS(app)
+# app.register_blueprint(posts_bp)
+
+# if __name__ == '__main__':
+#   app.debug=True
+#   host = '127.0.0.1'
+#   port = 5000
+#   os.environ['ENV'] = 'DEVELOPMENT'
+#   app.run(host=host, port=port)
+
+from app import create_app
 import os
-from flask import Flask
-from flask_cors import CORS
-from views.home import posts_bp
 from dotenv import load_dotenv
 
 load_dotenv()
-os.environ['ENV'] = 'PRODUCTION'
-
-app = Flask(__name__)
-CORS(app)
-
-app.register_blueprint(posts_bp)
+app = create_app()
 
 if __name__ == '__main__':
-  app.debug=True
-  host = '127.0.0.1'
-  port = 5000
-  os.environ['ENV'] = 'DEVELOPMENT'
-  app.run(host=host, port=port)
+    app.run(debug=True)
+    host = '127.0.0.1'
+    port = os.getenv('FLASK_RUN_PORT')
+    app.run(host=host, port=port)
---
+++ backend/app/config.py
@@ -0,0 +1,17 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+class Config:
+    if os.getenv('ENV') == 'DEVELOPMENT':
+        SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
+    else:
+        host = os.getenv('MYSQL_HOST')
+        user = user=os.getenv('MYSQL_USER')
+        password=os.getenv('MYSQL_PASSWORD')
+        database=os.getenv('MYSQL_DB')
+        
+        SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://{user}:{password}@{host}/{database}'
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
